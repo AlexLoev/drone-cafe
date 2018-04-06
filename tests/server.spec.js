550057dfd94cd2551bc3e18e233cd1ac7d5bb417
AlexLoev <alexloev1988@gmail.com>
2018-04-06 22:52:30 +0300

    running server on express and simple test

---
+++ tests/server.spec.js
@@ -0,0 +1,30 @@
+const supertest = require('supertest');
+const expect = require('chai').expect;
+const log = console.log;
+
+describe('Server functionality', () => {
+    let agent;
+    let app;
+
+    before(done => {
+        app = require('../src/app/server');
+        agent = supertest.agent(`http://localhost:${app.port}`);
+        done();
+    });
+
+    it('Answer for simple request to root "/"', done => {
+        agent
+            .get('/')
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    throw err
+                } else {
+                    done();
+                };
+            });
+    });
+    after(() => {
+        app.httpServer.close();
+    });
+});
